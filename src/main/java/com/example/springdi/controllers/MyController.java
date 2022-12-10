017f8c59714b11bb0b1f3f05592dd584d7b39b85
Faisal Ahmed <faisal.dev@gmail.com>
2022-12-10 15:39:47 +0300

    Create bean from context

---
+++ src/main/java/com/example/springdi/controllers/MyController.java
@@ -0,0 +1,10 @@
+package com.example.springdi.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+    public String sayHello(){
+        return "Hi Faisal";
+    }
+}
